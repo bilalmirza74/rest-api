272274c5b10b6d35318809d28ce0eb1abc401e5f
bilalmirza74 <mirzamunawaralibaig4@gmail.com>
2022-12-11 19:48:45 +0530

    .

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,13 @@
+const Product = require("../models/product");
+
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: "I am getAllProducts" });
+    const myData = await Product.find({});
+    res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async (req, res) => {
-    res.status(200).json({ msg: "I am getAllProductsTesting" });
+    const myData = await Product.find({});
+    res.status(200).json({ myData });
 };
 
 module.exports = { getAllProducts, getAllProductsTesting };
