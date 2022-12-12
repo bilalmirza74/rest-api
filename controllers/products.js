033fb68b2ddcc86e86921f97d8fae195b5ed183a
bilalmirza74 <mirzamunawaralibaig4@gmail.com>
2022-12-12 20:40:24 +0530

    .

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,11 @@ const getAllProducts = async (req, res) => {
 };
 
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Product.find({});
+    const myData = await Product.find(req.query);
+    console.log(
+        "~file: product.js ~ line 10 ~ getAllProductsTesting ~ req.query",
+        req.query
+    );
     res.status(200).json({ myData });
 };
 
