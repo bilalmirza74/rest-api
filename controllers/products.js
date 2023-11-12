387ea7ef6abb37d317b2eab0e5eee7f02ff0830c
bilalmirza74 <mirzamunawaralibaig4@gmail.com>
2023-11-12 13:16:01 +0530

    added: sothing data

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,27 @@
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-    const { company, name } = req.query;
+    const { company, name, featured, sort } = req.query;
     const queryObject = {};
 
+    if (featured){
+        queryObject.featured = featured;
+    }
     if (company) {
         queryObject.company = company;
     }
     if(name){
-        queryObject.name = name;
+        queryObject.name = {$regex: name, $options: "i"};
+    }
+    
+    let apiData = Product.find(queryObject);
+    if(sort){
+        const sortList = sort.split(",").join(" ");
+        apiData = apiData.sort(sortList);
     }
 
     try {
-        const myData = await Product.find(queryObject);
+        const myData = await apiData.sort(sort);
         res.status(200).json({ success: true, data: myData });
     } catch (error) {
         console.error("Error fetching all products:", error.message);
