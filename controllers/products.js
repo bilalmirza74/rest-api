d9112ef0f2d7a806e1d8efeaeb089a74dfee8cff
bilalmirza74 <mirzamunawaralibaig4@gmail.com>
2023-11-12 12:49:51 +0530

    fix: &second filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,16 @@
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-    const { company } = req.query;
+    const { company, name } = req.query;
     const queryObject = {};
 
     if (company) {
         queryObject.company = company;
     }
-    
+    if(name){
+        queryObject.name = name;
+    }
+
     try {
         const myData = await Product.find(queryObject);
         res.status(200).json({ success: true, data: myData });
